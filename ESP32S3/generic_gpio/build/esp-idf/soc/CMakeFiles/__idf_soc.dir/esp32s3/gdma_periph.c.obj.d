38f7b39a420401fbabcb922c986993426dee6619
jo12580@qq.com <jo12580@vip.qq.com>
2022-04-21 10:28:34 +0800

    Signed-off-by: jo12580@qq.com <jo12580@vip.qq.com>

---
+++ ESP32S3/generic_gpio/build/esp-idf/soc/CMakeFiles/__idf_soc.dir/esp32s3/gdma_periph.c.obj.d
@@ -0,0 +1,14 @@
+esp-idf/soc/CMakeFiles/__idf_soc.dir/esp32s3/gdma_periph.c.obj: \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/gdma_periph.c \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/include/soc/gdma_periph.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/soc_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/brownout_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/cpu_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/gpio_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/i2c_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/ledc_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/mpu_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/rtc_io_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/twai_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/uart_caps.h \
+ D:/Software/Visual_Studio_Code/ESP-ide/Espressif/frameworks/esp-idf-v4.4/components/soc/esp32s3/include/soc/periph_defs.h
